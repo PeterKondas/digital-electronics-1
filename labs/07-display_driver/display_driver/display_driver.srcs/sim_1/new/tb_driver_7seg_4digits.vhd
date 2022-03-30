ea6d01a4a8925cd46b5288c136898e64fb2aa008
student <student@PC-625.urel.feec.vutbr.cz>
2022-03-30 10:30:46 +0200

    skola

---
+++ labs/07-display_driver/display_driver/display_driver.srcs/sim_1/new/tb_driver_7seg_4digits.vhd
@@ -0,0 +1,91 @@
+------------------------------------------------------------
+--
+-- Template for 4-digit 7-segment display driver testbench.
+-- Nexys A7-50T, Vivado v2020.1.1, EDA Playground
+--
+-- Copyright (c) 2020-Present Tomas Fryza
+-- Dept. of Radio Electronics, Brno Univ. of Technology, Czechia
+-- This work is licensed under the terms of the MIT license.
+--
+------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+------------------------------------------------------------
+-- Entity declaration for testbench
+------------------------------------------------------------
+entity tb_driver_7seg_4digits is
+    -- Entity of testbench is always empty
+end entity tb_driver_7seg_4digits;
+
+------------------------------------------------------------
+-- Architecture body for testbench
+------------------------------------------------------------
+architecture testbench of tb_driver_7seg_4digits is
+
+    -- Local constants
+    constant c_CLK_100MHZ_PERIOD : time := 10 ns;
+
+    -- Local signals
+    signal s_clk_100MHz : std_logic;
+    signal s_reset : std_logic;
+    s_data0 : std_logic_vector(3 downto 0);
+    s_data1 : std_logic_vector(3 downto 0);
+    s_data2 : std_logic_vector(3 downto 0);
+    s_data3 : std_logic_vector(3 downto 0);
+    s_dpin  : std_logic_vector(3 downto 0);
+    s_dpout : std_logic;
+    s_seg   : std_logic_vector(6 downto 0);
+    s_dig   : std_logic_vector(3 downto 0);
+ 
+
+begin
+    -- Connecting testbench signals with driver_7seg_4digits
+    -- entity (Unit Under Test)
+    -- MAP I/O PORTS FROM ENTITY TO LOCAL SIGNALS
+    -- uut_driver_7seg_4digits : entity work....
+    clk => s_clk_100MHZ,
+    reset => s_reset,
+    data0_i => s_data0,
+    data1_i => s_data1,  
+    data2_i => s_data2,
+    data3_i => s_data3,
+    dp_i    => s_dpin,
+    dp_o => s_dpout,
+    seg_o => s_seg,
+    dig_o => s_dig
+    
+    p_clk_gen : process
+    begin
+        while now < 750 ns loop -- 75 periods of 100MHz clock
+            s_clk_100MHz <= '0';
+            wait for c_CLK_100MHZ_PERIOD / 2;
+            s_clk_100MHz <= '1';
+            wait for c_CLK_100MHZ_PERIOD / 2;
+        end loop;
+        wait;
+    end process p_clk_gen;
+
+    --------------------------------------------------------
+    -- Reset generation process
+    --------------------------------------------------------
+    -- WRITE YOUR CODE HERE AND ACTIVATE RESET FOR A WHILE
+    -- p_reset_gen : process
+    -- begin
+
+    --------------------------------------------------------
+    -- Data generation process
+    --------------------------------------------------------
+    
+   p_stimulus : process
+begin
+a_data3 <= x"3";
+a_data2 <= x"1";
+a_data1 <= x"4";
+a_data0 <= x"2";
+a_dpin  <= x"0111";
+report "Stimulus process finished" severity note;
+wait;
+end process p_stimulus;
+end architecture testbench;
